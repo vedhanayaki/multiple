324a106864c3bb348cdb8470868ad772c1ea7c21
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-19 02:42:13 -0700

    Add files via upload

---
+++ MultplicationTable.java
@@ -0,0 +1,16 @@
+package player;
+import java.util.Scanner;
+public class MultplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the number:");
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+for(int i=0;i<=10;i++){
+	System.out.println(n+"*"+i);
+	
+}
+	}
+
+}
